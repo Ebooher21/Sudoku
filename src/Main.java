e4474f332e5e01c10a74d862190e74bcd55be7cb
Elijah Booher <elijahdrake2003@gmail.com>
2025-05-13 22:11:57 -0700

    Made buttons
    Currently working to get the createNumButton method to return a selected value

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
+
+    public static void createSpace(int i, int k, JFrame frame, GridBagConstraints con){
+        con.gridx = k;
+        con.gridy = i;
+        con.ipadx = 50;
+        con.ipady = 50;
+        frame.add(new JButton(""),con);
+    }
+
+    public static void createNumButton(int num, JFrame frame, GridBagConstraints con, List<Integer> nums){
+        con.gridx = num;
+        con.gridy = 10;
+        con.ipadx = 50;
+        con.ipady = 50;
+
+        JButton button = new JButton(String.format("%d",num));
+        button.addActionListener(e ->{
+            int select = nums.get(num-1);
+        });
+        frame.add(button,con);
+    }
+
     public static void main(String[] args){
         // creating the main frame
         JFrame mainframe = new JFrame("Sudoku");
@@ -19,21 +44,25 @@ public class Main {
         intro.setSize(200,100);
 
         // add child widgets to parent frame
+        // add label
         con.gridx = 0;
         con.gridy = 0;
+        con.ipadx = 50;
+        con.ipady = 50;
         mainframe.add(intro,con);
 
-        //GridBagConstraints box = new GridBagConstraints();
-        //box.insets = new Insets(5,5,5,5);
+        // add empty spaces
         for(int i = 1; i <= 9; i++){
             for(int k = 1; k <= 9; k++){
-                con.gridx = k;
-                con.gridy = i;
-                JButton space = new JButton("");
-                space.setSize(50,50);
-                mainframe.add(space,con);
-
+                createSpace(i,k,mainframe,con);
         }}
+
+        List<Integer> nums = new ArrayList<>();
+        // add number selection buttons
+        for(int i = 1; i <= 9; i++){
+            nums.add(i);
+            createNumButton(i,mainframe,con,nums);
+        }
 
         // set the display
         mainframe.pack();
@@ -41,4 +70,5 @@ public class Main {
         mainframe.setLocationRelativeTo(null); // centers the frame
         mainframe.setVisible(true);
     }
+
 }
